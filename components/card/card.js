ed64d6a709888493eaa43ff799a298403d5eec7a
Dor Kuperman <dorkup@gmail.com>
2021-05-09 23:36:33 +0300

    styling

--- components/card/card.js
+++ components/card/card.js
@@ -1,28 +1,19 @@
+import { style } from '../../style'
+
 export default function MediaCard({ resturant }) {
   const { online, public_url, name, mainimage } = resturant
 
-  const style = {
-    card: {
-      width: '15%',
-      height: '15%'
-    },
-    text: {
-      fontSize: 'calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)))'
-    }
-  }
-
   return (
-    <div className="card is-square" style={style.card}>
+    <div className="card is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd" style={style.card}>
       <div className="card-image" />
       <figure className="image">
-        <img src={mainimage} alt="Placeholder image" />
+        <img style={style.borderRadius} src={mainimage} alt="Placeholder image" />
       </figure>
       <div />
-      <div className="card-content">
-        <div className="media">
-          <div className="media-content">
-            <p style={style.text} className="title is-4">{name}</p>
-            <p style={style.text} className="subtitle is-6">@johnsmith</p>
+      <div >
+        <div className="media has-text-centered">
+          <div className="media-content is-size-4-mobile is-size-4-touch is-size-4-tablet is-size-4-desktop is-size-4-widescreen is-size-4-fullhd">
+            <p>{name}</p>
           </div>
         </div>
       </div>
